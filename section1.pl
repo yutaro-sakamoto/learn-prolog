796666b6fa8e32c55a6cd8cd85aa11898fa79ac6
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-11-30 19:58:22 +0900

    update section1.pl

--- section1.pl
+++ section1.pl
@@ -1,4 +1,15 @@
 parent(tom, bob).
+parent(pam, bob).
+parent(tom, liz).
+parent(bob, ann).
+parent(bob, pat).
+parent(pat, jim).
 
 run_tests :-
-    parent(tom, bob1).
+    parent(tom, bob),
+    \+ parent(liz, pat),
+    \+ parent(tom, ben),
+    findall(X, parent(X, liz), Parents),
+    format('Parents of liz: ~w~n', [Parents]),
+    findall(X, parent(bob, X), Children),
+    format('Children of bob ~w~n', [Children]).
