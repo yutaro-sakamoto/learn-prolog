1b53feab8fccf7ad97ba97df3ec56c7d46b1b0d2
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-12-08 17:13:50 +0900

    add: section4.pl

---
+++ section4.pl
@@ -0,0 +1,13 @@
+family(
+    person(tom, fox, date(7, may, 1950), works(bbc, 15200)),
+    person(ann, fox, date(9, may, 1951), unemployed),
+    [
+        person(pat, fox, date(5, may, 1973), unemployed),
+        person(jim, fox, date(5, may, 1973), unemployed)
+    ]
+).
+
+
+run_tests :-
+    family(_, person(Name, Surname, _, _), [_, _ | _]),
+    format('Name, Surname: ~w ~w ~n', [Name, Surname]).
