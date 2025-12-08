3b6ee50e20280ef0550b4909c807ff08b45dfb71
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-12-08 17:31:56 +0900

    wip: wip

--- section4.pl
+++ section4.pl
@@ -7,7 +7,32 @@ family(
     ]
 ).
 
+husband(X) :-
+    family(X, _, _).
+
+wife(X) :-
+    family(_, X, _).
+
+child(X) :-
+    family(_, _, Children),
+    member(X, Children).
+
+member(X, [X | _]).
+member(X, [_ | L]) :-
+    member(X, L).
+
+exists(Person) :-
+    husband(Person);
+    wife(Person);
+    child(Person).
+
+dateofbirth(person(_, _, Date, _), Date).
+salary(person(_, _, _, works(_, S)), S).
+salary(person(_, _, _, unemployed), 0).
 
 run_tests :-
     family(_, person(Name, Surname, _, _), [_, _ | _]),
-    format('Name, Surname: ~w ~w ~n', [Name, Surname]).
+    format('Name, Surname: ~w ~w ~n', [Name, Surname]),
+    findall(N-S, exists(person(N, S, _, _)), AllPerson),
+    format('AllPerson: ~w~n', [AllPerson])
+    .
