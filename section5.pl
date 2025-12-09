652349c2b5ac3326a5a4255bd1eb7192a3f202c3
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-12-09 20:12:14 +0900

    wip: wip

--- section5.pl
+++ section5.pl
@@ -31,11 +31,39 @@ likes(mary, X) :-
 likes(mary, X) :-
     animal(X).
 
+different(X, Y) :-
+    X = Y, !, fail;
+    true.
+
+likes2(mary, X) :-
+    animal(X),
+    not(snake(X)).
+
+different2(mary, X) :-
+    animal(X),
+    not(snake(X)).
+
+solution([]).
+solution([X/Y | Others]) :-
+    solution(Others),
+    member2(Y, [1,2,3,4,5,6,7,8]),
+    not(attacks(X/Y, Others)).
+
+attacks(X/Y, Others) :-
+    member2(X1/Y1, Others),
+    (Y1 = Y;
+     Y1 is Y + X1 - X;
+     Y1 is Y - X1 + X).
+
+member2(X, [X | _]).
+member2(X, [_ | L]) :- member2(X, L).
+
 run_tests :-
     \+ (f(1, Y), 2 < Y),
     mymax(1, 2, 2),
     mymax(2, 1, 2),
     add(a, [b, c, X], L),
     format('X: ~w~n', [X]),
-    format('L: ~w~n', [L])
+    format('L: ~w~n', [L]),
+    solution([1/_, 2/_, 3/_, 4/_, 5/_, 6/_, 7/_, 8/_])
     .
