a164c984f2b5cc856f32434cb403d9e36566ab59
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-12-10 23:49:10 +0900

    wip: wip

--- cube2.pl
+++ cube2.pl
@@ -25,3 +25,37 @@ doline([]).
 doline([X | L]) :-
     write(X), tab(1),
     doline(L).
+
+bars([N | L]) :-
+    stars(N), nl,
+    bars(L).
+
+stars(N) :-
+    N > 0,
+    write(*),
+    N1 is N - 1,
+    stars(N1).
+
+stars(N) :-
+    N =< 0.
+
+processfile :-
+    read(Term),
+    process1(Term).
+
+process1(end_of_file) :- !.
+
+process1(Term) :-
+    treat(Term),
+    processfile.
+
+showfile(N) :-
+    read(Term),
+    show(Term, N).
+
+show(end_of_file, _) :- !.
+
+show(Term, N) :-
+    write(N), tab(2), write(Term),
+    N1 is N + 1,
+    showfile(N1).
