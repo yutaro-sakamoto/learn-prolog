52ae7e86c6c9c2c9ba92b8b33454b97927b7dd36
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-12-10 23:37:59 +0900

    add: cube2.pl

--- cube2.pl
+++ cube2.pl
@@ -10,3 +10,18 @@ process(N) :-
     write('Cube of'), write(N), write(' is '),
     write(C), nl,
     cube.
+
+writelist([]).
+writelist([X | L]) :-
+    write(X), nl,
+    writelist(L).
+
+writelist2([[a,b,c], [d,e,f], [g,h,i]]).
+writelist2([L | LL]) :-
+    doline(L), nl,
+    writelist(LL).
+
+doline([]).
+doline([X | L]) :-
+    write(X), tab(1),
+    doline(L).
