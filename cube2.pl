1f073b937975eb118849a2063f9858fd8f1bbb20
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-12-10 23:27:39 +0900

    add: cube2.pl

---
+++ cube2.pl
@@ -0,0 +1,12 @@
+cube :-
+    write('Next item, please: '),
+    read(X),
+    process(X).
+
+process(stop) :- !.
+
+process(N) :-
+    C is N * N * N,
+    write('Cube of'), write(N), write(' is '),
+    write(C), nl,
+    cube.
